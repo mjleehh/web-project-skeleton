e4d29085855deb4d4672b7bb7f1dc6940174c6a8
Michael Jonathan Lee <code.mjlee@gmail.com>
2019-09-30 11:21:19 +0200

    Updated dependencies for React project.

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
